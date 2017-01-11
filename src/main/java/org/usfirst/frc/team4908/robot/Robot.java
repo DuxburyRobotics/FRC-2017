173089225303583587b139c85732a21261e2994a
Billy Siegener <willsiggs@gmail.com>
2017-01-11 15:57:33 -0500

    Fixing

--- src/main/java/org/usfirst/frc/team4908/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4908/robot/Robot.java
@@ -2,9 +2,6 @@
 package org.usfirst.frc.team4908.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot
 {
