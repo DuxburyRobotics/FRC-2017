939ea3ff4e8aaa9b7d0981aef983b45959501a37
Billy Siegener <willsiggs@gmail.com>
2017-01-21 20:14:11 -0500

    Made a simple program for running motors on prototypes.

--- src/main/java/org/usfirst/frc/team4908/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4908/robot/Robot.java
@@ -1,33 +1,24 @@
-
 package org.usfirst.frc.team4908.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class Robot extends IterativeRobot
 {
+    private SpeedController controller1;
+    private Joystick input;
 
     public void robotInit()
     {
-    }
+        controller1 = new VictorSP(0);
 
-    public void autonomousInit()
-    {
-
-    }
-
-    public void autonomousPeriodic()
-    {
-
+        input = new Joystick(0);
     }
 
     public void teleopPeriodic()
     {
-        
+        controller1.set(input.getRawAxis(0));
     }
-
-    public void testPeriodic()
-    {
-    
-    }
-    
 }
