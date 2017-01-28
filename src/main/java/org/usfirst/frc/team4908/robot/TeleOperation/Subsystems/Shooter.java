680877c9f0524373cd9cbafc59576d3fe7c99f1c
Billy Siegener <willsiggs@gmail.com>
2017-01-27 22:14:31 -0500

    quick fix for shooter example (made it set motor values every update, defaulting to 0)

--- src/main/java/org/usfirst/frc/team4908/robot/TeleOperation/Subsystems/Shooter.java
+++ src/main/java/org/usfirst/frc/team4908/robot/TeleOperation/Subsystems/Shooter.java
@@ -16,11 +16,14 @@ public class Shooter implements ISubsystem
     SensorInput si;
     DuxPID PID;
 
+    private double setValue;
+
     public Shooter()
     {
         di = new DriverInput();
         si = new SensorInput();
         PID = new DuxPID(0.0, 0.0, 0.0, 0.02);
+        setValue = 0.0;
     }
 
     public void calculate()
@@ -32,9 +35,14 @@ public class Shooter implements ISubsystem
         {
             PID.reset();
             PID.setSetPoint(targetRPM);
-            ro.setShooter(PID.calculate(si.getShooterSpeed()));
+            setValue = PID.calculate(si.getShooterSpeed());
+        }
+        else if(!di.getShooterButton())
+        {
+            setValue = 0.0;
         }
 
+        ro.setShooter(setValue);
     }
 
     public void disable() {
