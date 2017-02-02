9f9329aadc72cafe19399c01f37362f78d32eaf3
Billy Siegener <willsiggs@gmail.com>
2017-02-02 15:36:15 -0500

    basic drive code

--- src/main/java/org/usfirst/frc/team4908/robot/Subsystems/Drive.java
+++ src/main/java/org/usfirst/frc/team4908/robot/Subsystems/Drive.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team4908.robot.Subsystems;
 
 
+import org.usfirst.frc.team4908.robot.Input.DriverInput;
+import org.usfirst.frc.team4908.robot.Input.SensorInput;
+import org.usfirst.frc.team4908.robot.TeleOperation.RobotOutput;
+
 /**
  * Created by kyleknobloch on 1/24/17,
  * For
@@ -10,16 +14,35 @@ package org.usfirst.frc.team4908.robot.Subsystems;
 public class Drive implements ISubsystem
 {
 
-    public Drive() {
+    private DriverInput di;
+    private SensorInput si;
 
+    private double driveX;
+    private double driveRot;
+
+    private RobotOutput ro;
+
+    public Drive()
+    {
+        di = new DriverInput();
+        si = new SensorInput();
+        ro = new RobotOutput();
+
+        driveX = 0.0;
+        driveRot = 0.0;
     }
 
     // TELEOPS
-    public void calculate() {
+    public void calculate()
+    {
+        driveX = di.getDriveX();
+        driveRot = di.getDriveRot();
 
+        ro.setDriveMotors(driveX, driveRot);
     }
 
-    public void disable() {
+    public void disable()
+    {
 
     }
 
--- src/main/java/org/usfirst/frc/team4908/robot/TeleOperation/RobotOutput.java
+++ src/main/java/org/usfirst/frc/team4908/robot/TeleOperation/RobotOutput.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team4908.robot.TeleOperation;
 
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * Created by kyleknobloch on 1/24/17,
@@ -15,9 +18,32 @@ public class RobotOutput
     private SpeedController shooterThree;
     private SpeedController shooterFour;
 
+    private SpeedController frontLeftMotor;
+    private SpeedController rearLeftMotor;
+    private SpeedController frontRightMotor;
+    private SpeedController rearRightMotor;
+
+    private RobotDrive rd;
+
+
     public RobotOutput()
     {
+        shooterOne = new VictorSP(0);
+        shooterTwo = new VictorSP(1);
+        shooterThree = new VictorSP(2);
+        shooterFour = new VictorSP(3);
 
+        frontLeftMotor = new CANTalon(0);
+        rearLeftMotor = new CANTalon(1);
+        frontRightMotor = new CANTalon(2);
+        rearRightMotor = new CANTalon(3);
+
+        rd = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+    }
+
+    public void setDriveMotors(double x, double r)
+    {
+        rd.arcadeDrive(x, r);
     }
 
     public void setShooter(double val)
