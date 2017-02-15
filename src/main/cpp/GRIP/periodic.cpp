6c9dfd61fb8265608327286e6bd4cf530ce0b014
fredghostkyle <fredghostkyle@yahoo.com>
2017-02-15 12:02:22 -0500

    cpp runner

---
+++ src/main/cpp/GRIP/periodic.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#Include <opencv/networktables.h?
+
+
+int main() {
+
+    NetworkTable *table;
+
+    table = NetworkTable::initialise("IP")
+    table = NetworkTable::getDatabase("database");
+
+
+
+    while (1) {
+        //post variables to NT
+    }
+
+    return 0
+}
