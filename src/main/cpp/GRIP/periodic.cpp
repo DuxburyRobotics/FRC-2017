0ff9ff73693b95dbd7eb3ae433ec74b09e3633c1
fredghostkyle <fredghostkyle@yahoo.com>
2017-02-16 08:35:22 -0500

    no message

--- src/main/cpp/GRIP/periodic.cpp
+++ src/main/cpp/GRIP/periodic.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
-//#Include <opencv/networktables.h?
+//#include <WPILib.h>
+//#include <opencv/networktables.h>
 
 
 int main() {
 
     NetworkTable *table;
 
-    table = NetworkTable::initialise("IP")
+    table = NetworkTable::SetClient();
+    table = NetworkTable::SetIP("10.49.8.10"); //roboRIO
+    table = NetworkTable::Initialize();
     table = NetworkTable::getDatabase("database");
 
 
